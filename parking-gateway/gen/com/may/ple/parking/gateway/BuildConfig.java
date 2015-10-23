168e343ae48a8c8c4895cd401e33020d65b3831a
mayfender <mayfender@mayfender-PC>
2015-10-23 18:21:39 +0700

    update

---
+++ parking-gateway/gen/com/may/ple/parking/gateway/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.may.ple.parking.gateway;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ parking-gateway/src/main/java/com/may/ple/parking/gateway/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.may.ple.parking.gateway;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class LoginActivity extends SherlockActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    
+    
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(com.may.ple.parking.gateway.R.menu.main, menu);
+	return true;
+    }*/
+
+}
+
