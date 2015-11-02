5dda82399d4017bcab6dabcf025e3592ffb26f67
mayfender <mayfender@mayfender-PC>
2015-11-02 08:41:49 +0700

    update

--- parking-gateway/src/main/java/com/may/ple/parking/gateway/activity/GateOutActivity.java
+++ parking-gateway/src/main/java/com/may/ple/parking/gateway/activity/GateOutActivity.java
@@ -1,14 +1,12 @@
 package com.may.ple.parking.gateway.activity;
 
+import net.sourceforge.zbar.android.CameraTest.CameraTestActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 public class GateOutActivity extends SherlockActivity {
 
@@ -19,29 +17,20 @@ public class GateOutActivity extends SherlockActivity {
 	}
 
 	public void onClick(View v) {
-		IntentIntegrator integrator = new IntentIntegrator(this);
-        integrator.addExtra("SCAN_WIDTH", 1000);
-        integrator.addExtra("SCAN_HEIGHT", 480);
-        integrator.addExtra("SCAN_MODE", "QR_CODE_MODE,PRODUCT_MODE");
-        //customize the prompt message before scanning
-        integrator.addExtra("PROMPT_MESSAGE", "Scanner Start!");
-        integrator.initiateScan(IntentIntegrator.PRODUCT_CODE_TYPES);
+		Intent i = new Intent(this, CameraTestActivity.class);
+        startActivityForResult(i, 1);
 	}
 
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-
-		if (scanningResult != null) {
-			String scanContent = scanningResult.getContents();
-			String scanFormat = scanningResult.getFormatName();
-			
-			TextView showScan = (TextView)findViewById(R.id.show_scan);
-			showScan.setText("Content: " + scanContent +", Format: " + scanFormat);
-		} else {
-			Toast toast = Toast.makeText(getApplicationContext(),"No scan data received!", Toast.LENGTH_SHORT);
-			toast.show();
-		}
-
-	}
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) { 
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK){
+                String result=data.getStringExtra("result");
+                Toast.makeText(this, result, Toast.LENGTH_LONG).show();               
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        } 
+    }
 
 }
